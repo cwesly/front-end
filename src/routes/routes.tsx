ae456f268f5200ae32e02e546610c9442d2bae5a
cwesly <cauawesly@outlook.com>
2024-09-14 16:36:24 -0300

    refactor: remove app route.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/loginPage/login";
-import App from "../App";
 import Register from "../pages/register/register";
 import HomePage from "../pages/homePage/homePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
     element: <LoginPage />,
   },
   {
