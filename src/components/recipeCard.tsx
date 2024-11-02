679e3939a7a3f334db3ec9c91054e85c24c11070
cwesly <cauawesly@outlook.com>
2024-11-02 16:40:19 -0300

    feat: add and remove favorite logic

--- src/components/recipeCard.tsx
+++ src/components/recipeCard.tsx
@@ -1,21 +1,49 @@
 import { useNavigate } from "react-router-dom";
 import { BASE_IMAGE_URL } from "../constants";
 import { IRecipe } from "../types/types";
+import FavoriteCheckbox from "./favoriteCheckbox";
+import { useContext, useEffect, useState } from "react";
+import useFavorite from "../hooks/useFavorites";
+import FavoritesContext from "../context/favoritesContext";
 
 type RecipeCardProp = {
   recipe: IRecipe;
 };
 
 const RecipeCard = ({ recipe }: RecipeCardProp) => {
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const { addFavorite, removeFavorite, getFavorites } = useFavorite();
   const navigate = useNavigate();
+  const { favorites } = useContext(FavoritesContext);
+
   const handleClick = () => {
     navigate(`/recipe/${recipe.ID}`);
   };
+
+  const handleFavorite = () => {
+    if (!isFavorite) {
+      addFavorite(recipe.ID);
+      setIsFavorite(true);
+    } else {
+      removeFavorite(recipe.ID);
+      setIsFavorite(false);
+    }
+    getFavorites();
+  };
+
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
+  useEffect(() => {
+    setIsFavorite(favorites.some((favorite) => favorite.ID === recipe.ID));
+  }, [favorites]);
 
   return (
     <div>
       <img src={`${BASE_IMAGE_URL}${recipe.image}.jpg`} alt="recipe"></img>
       <p>{recipe.title}</p>
+      <FavoriteCheckbox checked={isFavorite} setChecked={handleFavorite} />
       <button onClick={handleClick}>Details</button>
     </div>
   );
