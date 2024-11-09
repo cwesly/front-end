b8fe859b69618f0bcce935585a3770d75613caca
cwesly <cauawesly@outlook.com>
2024-11-09 09:40:35 -0300

    feat: add page logic

--- src/hooks/useRecipes.tsx
+++ src/hooks/useRecipes.tsx
@@ -3,19 +3,26 @@ import { IRecipe } from "../types/types";
 import { BASE_URL } from "../constants";
 import axios from "axios";
 
-const useRecipe = () => {
+const useRecipe = (page: number) => {
   const [recipes, setRecipes] = useState<IRecipe[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
   useEffect(() => {
     const getRecipe = async () => {
-      const baseGetUrl = `${BASE_URL}/recipes/page/1`;
+      const baseGetUrl = `${BASE_URL}/recipes/page/${page}`;
       setIsLoading(true);
-      const response = await axios.get(baseGetUrl);
-      setRecipes(response.data.recipes);
+      try {
+        const response = await axios.get(baseGetUrl);
+        setRecipes(response.data.recipes);
+      } catch (error) {
+        console.error("Error fetching recipes", error);
+      }
       setIsLoading(false);
     };
     getRecipe();
-  }, []);
+  }, [page]);
+
   return { recipes, isLoading };
 };
+
 export default useRecipe;
